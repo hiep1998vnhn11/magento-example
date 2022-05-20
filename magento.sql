d7d53d5926373cfc8cc00cfbbb1306d0a1019f07
Hiệp Trần <hiep1998vnhn11@gmail.com>
2022-05-20 15:07:14 +0700

    add db

---
+++ magento.sql
@@ -0,0 +1,12 @@
+Downloading n98-magerun2.phar, just a moment...
+n98-magerun2.phar: OK
+
+[33mIn ErrorHandler.php line 61:[39m
+[37;41m                                                                               [39;49m
+[37;41m  Deprecated Functionality: strlen(): Passing null to parameter #1 ($string)   [39;49m
+[37;41m  of type string is deprecated in phar:///var/www/html/bin/n98-magerun2.phar/  [39;49m
+[37;41m  src/N98/Magento/Command/Database/Compressor/Uncompressed.php on line 49      [39;49m
+[37;41m                                                                               [39;49m
+
+[32mdb:dump [--connection CONNECTION] [-t|--add-time [ADD-TIME]] [-c|--compression COMPRESSION] [--only-command] [--print-only-filename] [--dry-run] [--set-gtid-purged-off] [--no-single-transaction] [--human-readable] [--git-friendly] [--add-routines] [--no-tablespaces] [--stdout] [-s|--strip [STRIP]] [-e|--exclude [EXCLUDE]] [-i|--include [INCLUDE]] [-f|--force] [--keep-column-statistics] [-h|--help] [-q|--quiet] [-v|vv|vvv|--verbose] [-V|--version] [--ansi] [--no-ansi] [-n|--no-interaction] [--root-dir [ROOT-DIR]] [--skip-config] [--skip-root-check] [--skip-core-commands] [--skip-magento-compatibility-check] [--] <command> [<filename>][39m
+
